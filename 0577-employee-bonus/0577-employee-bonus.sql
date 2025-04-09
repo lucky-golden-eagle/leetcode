f1842786f4aac12c69cf279d2713d0b4e3b83be3
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-04-09 19:31:40 -0400

    Time: 950 ms (89.12%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
 SELECT e.name, b.bonus
 FROM Employee e
-LEFT JOIN Bonus b ON e.empId = b.empId
-WHERE b.bonus < 1000 OR b.bonus IS NULL;
+INNER JOIN Bonus b ON e.empId = b.empId
+WHERE b.bonus < 1000
+UNION ALL
+SELECT e.name, NULL AS bonus
+FROM Employee e
+WHERE e.empId NOT IN (SELECT empId FROM Bonus);
