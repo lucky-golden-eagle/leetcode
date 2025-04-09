e3c07df271ad0d9444fc70a424b1d7406a26b5be
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-04-09 09:24:26 -0400

    Time: 52 ms (71.48%), Space: 32 MB (83.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,30 +1,9 @@
 class Solution:
-    def findMaxSubarrayRecursive(self, nums: List[int], low: int, high: int) -> int:
-        if low == high:
-            return nums[low]
-
-        mid = (low + high) // 2
-
-        left_max = self.findMaxSubarrayRecursive(nums, low, mid)
-        right_max = self.findMaxSubarrayRecursive(nums, mid + 1, high)
-
-        left_border_sum = -math.inf
-        current_sum = 0
-        for i in range(mid, low - 1, -1):
-            current_sum += nums[i]
-            left_border_sum = max(left_border_sum, current_sum)
-
-        right_border_sum = -math.inf
-        current_sum = 0
-        for i in range(mid + 1, high + 1):
-            current_sum += nums[i]
-            right_border_sum = max(right_border_sum, current_sum)
-            
-        cross_max = left_border_sum + right_border_sum
-
-        return max(left_max, right_max, cross_max)
-
     def maxSubArray(self, nums: List[int]) -> int:
-        # Constraints state nums.length >= 1, so no need to check for empty
-        return self.findMaxSubarrayRecursive(nums, 0, len(nums) - 1)
+        answer, temp_sum = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            temp_sum = max(temp_sum + nums[i], nums[i])
+            if temp_sum > answer:
+                answer = temp_sum
+        return answer
         
