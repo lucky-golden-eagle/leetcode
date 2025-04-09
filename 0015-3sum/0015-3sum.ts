11127cd7811a9a9a0447ec0e7c5c34767aaaf1d6
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-04-09 16:56:06 -0400

    Time: 37 ms (51.78%), Space: 73.9 MB (38.78%) - LeetHub

---
+++ 0015-3sum/0015-3sum.ts
@@ -0,0 +1,35 @@
+function threeSum(nums: number[]): number[][] {
+    nums.sort((a, b) => a - b);
+  const result: number[][] = [];
+  const n = nums.length;
+
+  for (let i = 0; i < n - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let left = i + 1;
+    let right = n - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          right--;
+        }
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return result;
+};
