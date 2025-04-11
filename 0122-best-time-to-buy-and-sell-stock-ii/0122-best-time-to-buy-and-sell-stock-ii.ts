d5e23e517856af222057d676342a6f39281a763d
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-04-11 13:12:56 -0400

    Time: 2 ms (36.48%), Space: 56 MB (67.71%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,9 @@
+function maxProfit(prices: number[]): number {
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length - 1; i++) {
+        if (prices[i + 1] > prices[i]) {
+            maxProfit += prices[i + 1] - prices[i];
+        }
+    }
+    return maxProfit;
+};
