586b2a9347aa9e140529d7f37b490e8b254941ca
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-05-15 06:14:19 -0500

    Time: 5 ms (81.95%), Space: 57.4 MB (77.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-   let maxLen = 0;
-  let start = 0;
-  let charMap = new Map();
+    let maxLen = 0;
+    let start = 0;
+    let charMap = new Map();
 
-  for (let end = 0; end < s.length; end++) {
-    const currentChar = s.charAt(end);
-    if (charMap.has(currentChar) && charMap.get(currentChar) >= start) {
-      start = charMap.get(currentChar) + 1;
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s.charAt(end);
+        if (charMap.has(currentChar) && charMap.get(currentChar) >= start) {
+        start = charMap.get(currentChar) + 1;
+        }
+        charMap.set(currentChar, end);
+        maxLen = Math.max(maxLen, end - start + 1);
     }
-    charMap.set(currentChar, end);
-    maxLen = Math.max(maxLen, end - start + 1);
-  }
 
-  return maxLen;
+    return maxLen;
 };
