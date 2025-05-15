45fa8b0b7d8c40c5092226f092cf4b5aace486df
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-05-15 05:50:06 -0500

    Time: 9 ms (45.6%), Space: 23.3 MB (6.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         if not s:
             return True
         new_string = ''.join(char.lower() for char in s if char.isalpha() or char.isdigit())
