0bd5d36643a7d2316ca99718279fa80f734f3d96
Golden_E@gle <112952011+lucky-golden-eagle@users.noreply.github.com>
2025-05-15 06:13:25 -0500

    Time: 4 ms (54.28%), Space: 13.2 MB (61.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,19 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        resultList = []
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] != target:
-                    continue;
-                resultList = [i, j]
-        return resultList
-                    
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in lookup:
+                return [lookup[diff], i]
+            lookup[num] = i
+
+
+
+
+
+
