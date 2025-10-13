0068406b4945682b38ed490948a5b2636af5f604
Golden_E@gle <devstev2022@gmail.com>
2025-10-13 06:47:35 -0500

    Time: 0 ms (100%), Space: 62.6 MB (58.69%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    let cur = root;
+    while (cur) {
+        if (cur.val === val) return cur;
+        cur = val < cur.val ? cur.left : cur.right;
+    }
+    return null;
+};
